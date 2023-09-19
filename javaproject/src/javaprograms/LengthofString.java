dd486b9d4300e14f9df6ffc22812d8c2f7a6e0cd
moni <monimanoj7725@gmail.com>
2023-09-19 16:17:51 +0530

    task8

---
+++ javaproject/src/javaprograms/LengthofString.java
@@ -0,0 +1,11 @@
+package javaprograms;
+
+public class LengthofString {
+	public static void main(String[] args) {
+		String n="GuviGeek";
+		int l=n.length();
+		System.out.println(l);
+		
+	}
+
+}
